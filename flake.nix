28bc6ed1406a7c7b7d84e7913cd72ea5c0fec42f
DFW2006 <dfwatson2006@gmail.com>
2026-06-01 17:59:10 +0200

    Initial NixOS Flake setup with sxwm

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "My NixOS Rice Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
